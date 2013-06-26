f40653a4ae9058f7f4fe96b67f27d67895bfb3fd
Ken Lu <ken@seeice.com>
2013-06-26 09:10:00 -0400

    init code base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rank::Application.config.secret_token = '47b794d1b162a0cdb5cc58fcbc364c5c8a3d6874aff0626b4e6c108f7f4e5839726d1cfd374d3b75f1bd1c799175de0a80bd435c087ac42ce6aadc32ea896681'
