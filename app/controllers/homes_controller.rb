0640063f2986fb6fd7f63bf4f7c53208a6d51ef2
Ken Lu <ken@seeice.com>
2013-06-27 09:20:26 -0400

    add function of caculating commit

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,9 +1,25 @@
+require 'open-uri'
 class HomesController < ApplicationController
+  before_filter :generate_client
 
   def index
     @users = ["mouse-lin", "williamherry", "biorainy", "RyanZhu1024"].map do |user|
-      Octokit.user user
+      @client.user user
+    end
+
+    @repos_list = @users.map do |user|
+      repos = JSON.parse(open(user.repos_url).read)
+      binding.pry
+      commit_count = repos.inject(0) do |sum, repo|
+        num = JSON.parse(open(repo['commits_url'].gsub(/\{.*\}/,'')).read).count rescue 0
+        sum += num 
+        #TODO please add commit commits.first['commit']['committer']['date']
+      end
+      {user: user, commit_count: commit_count}
     end
   end
 
+  def generate_client
+    @client = Octokit::Client.new(:login => "ken0", :password => "password9")
+  end
 end
