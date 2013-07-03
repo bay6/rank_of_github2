20c813da2c4003a9d97bb1e36da87d2307348294
Ken Lu <ken@seeice.com>
2013-07-03 09:11:37 -0400

    added method to get all repos of the bay6

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130630155427) do
+ActiveRecord::Schema.define(:version => 20130703125824) do
 
   create_table "contributes", :force => true do |t|
     t.integer  "user_id"
@@ -40,6 +40,17 @@ ActiveRecord::Schema.define(:version => 20130630155427) do
 
   add_index "members", ["email"], :name => "index_members_on_email", :unique => true
   add_index "members", ["reset_password_token"], :name => "index_members_on_reset_password_token", :unique => true
+
+  create_table "projects", :force => true do |t|
+    t.string   "uid"
+    t.string   "name"
+    t.string   "description"
+    t.string   "html_url"
+    t.string   "avatar_url"
+    t.string   "starred_url"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
