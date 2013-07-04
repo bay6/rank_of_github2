cc1f73044fd923c1707c6a9617fe2e5eced615ff
Ken Lu <ken@seeice.com>
2013-07-03 21:49:59 -0400

    updated one of the column

--- test/functional/commits_controller_test.rb
+++ test/functional/commits_controller_test.rb
@@ -18,7 +18,7 @@ class CommitsControllerTest < ActionController::TestCase
 
   test "should create commit" do
     assert_difference('Commit.count') do
-      post :create, commit: { author_date: @commit.author_date, author_id: @commit.author_id, author_login: @commit.author_login, committer_email: @commit.committer_email, committer_name: @commit.committer_name, html_url: @commit.html_url, project_id: @commit.project_id, sha: @commit.sha }
+      post :create, commit: { commit_date: @commit.commit_date, author_id: @commit.author_id, author_login: @commit.author_login, committer_email: @commit.committer_email, committer_name: @commit.committer_name, html_url: @commit.html_url, project_id: @commit.project_id, sha: @commit.sha }
     end
 
     assert_redirected_to commit_path(assigns(:commit))
@@ -35,7 +35,7 @@ class CommitsControllerTest < ActionController::TestCase
   end
 
   test "should update commit" do
-    put :update, id: @commit, commit: { author_date: @commit.author_date, author_id: @commit.author_id, author_login: @commit.author_login, committer_email: @commit.committer_email, committer_name: @commit.committer_name, html_url: @commit.html_url, project_id: @commit.project_id, sha: @commit.sha }
+    put :update, id: @commit, commit: { commit_date: @commit.commit_date, author_id: @commit.author_id, author_login: @commit.author_login, committer_email: @commit.committer_email, committer_name: @commit.committer_name, html_url: @commit.html_url, project_id: @commit.project_id, sha: @commit.sha }
     assert_redirected_to commit_path(assigns(:commit))
   end
 
